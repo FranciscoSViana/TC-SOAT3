9afe483089231ad32b3550cd07324c5a45a1bee2
franciscosviana <franciscosantos29@gmail.com>
2023-09-22 15:37:48 -0300

    commit inicial

---
+++ src/test/java/com/tech/challenge/soat/SoatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.challenge.soat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
