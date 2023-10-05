0cc8162b7b9265b2cc29988362679de131f7e686
unknown <vinni.jr@gmail.com>
2023-10-05 19:53:40 -0300

    Refactory do pacote de excptions

---
+++ src/main/java/com/tech/challenge/soat/core/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package com.tech.challenge.soat.core.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
