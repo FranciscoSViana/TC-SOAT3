9b76f05f2467b3f128da29fe057b4b8bd55bf55d
VinniJr <vinni.jr@gmail.com>
2024-01-15 19:13:04 -0300

    Ajustes

--- src/main/java/com/tech/challenge/soat/adapters/controllers/Webhook.java
+++ src/main/java/com/tech/challenge/soat/adapters/controllers/Webhook.java
@@ -1,13 +1,10 @@
 package com.tech.challenge.soat.adapters.controllers;
 
 import com.tech.challenge.soat.domain.models.PedidoModel;
-import com.tech.challenge.soat.domain.ports.out.pagamento.PagamentoPort;
 import com.tech.challenge.soat.domain.services.PedidoService;
 import com.tech.challenge.soat.domain.utils.JsonUtil;
-import com.tech.challenge.soat.infra.client.MercadoPagoClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +26,6 @@ public class Webhook {
         String id = jsonUtil.obterValorChaveJson(payload, "id");
 
         PedidoModel pedido = service.obterPorIdPagamentoMP(id);
-
-
 
         return "Endereço fornecido pelo ngrok: " + id;
     }
