83bde0d37149ae7815e1482e2cd8f892cbe55511
Francisco Viana <franciscosantos29@gmail.com>
2024-01-23 21:32:01 -0300

    Correção do import.sql e adição dos manifestos k8s

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,99 +1,49 @@
-insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao)
-values ('1', 'Francisco Viana', '12345678900', 'teste@gmail.com', '11911111111', utc_timestamp, utc_timestamp, true);
-insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao)
-values ('2', 'Vinicius Junior', '00987654321', 'java@gmail.com', '11922222222', utc_timestamp, utc_timestamp, true);
-insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao)
-values ('3', 'Isaque Saraiva', '00987654321', 'spring@gmail.com', '11933333333', utc_timestamp, utc_timestamp, true);
-insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao)
-values ('4', 'Fernanda Freitas', '00987654321', 'docker@gmail.com', '11944444444', utc_timestamp, utc_timestamp, true);
-insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao)
-values ('5', 'Gabriel Sordi', '00987654321', 'mysql@gmail.com', '11955555555', utc_timestamp, utc_timestamp, true);
+insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao) values ('1', 'Francisco Viana', '12345678900', 'teste@gmail.com', '11911111111', utc_timestamp, utc_timestamp, true);
+insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao) values ('2', 'Vinicius Junior', '00987654321', 'java@gmail.com', '11922222222', utc_timestamp, utc_timestamp, true);
+insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao) values ('3', 'Isaque Saraiva', '00987654321', 'spring@gmail.com', '11933333333', utc_timestamp, utc_timestamp, true);
+insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao) values ('4', 'Fernanda Freitas', '00987654321', 'docker@gmail.com', '11944444444', utc_timestamp, utc_timestamp, true);
+insert into cliente_model (id, nome, cpf, email, telefone, data_hora_criacao, data_hora_alteracao, situacao) values ('5', 'Gabriel Sordi', '00987654321', 'mysql@gmail.com', '11955555555', utc_timestamp, utc_timestamp, true);
 
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('1', 'Hot Dog', 'Pão de hot dog, salsicha, vinagrete, purê e batata palha', '', 12.50, 'Lanche', utc_timestamp,
-        utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('2', 'Mega Hamburguer', 'Pão de hamburguer, duas carnes, ketchup, maionese, bacon, tomate, picles', '', 20.00,
-        'Lanche', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('3', 'Pastel', 'Pastel com o sabor a escolha do cliente', '', 10.00, 'Lanche', utc_timestamp, utc_timestamp,
-        true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('4', 'Coxinha', 'Coxinha de frango com catupiry', '', 2.50, 'Lanche', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('5', 'Risoli', 'Risoli de presunto com queijo', '', 3.00, 'Lanche', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('1', 'Hot Dog', 'Pão de hot dog, salsicha, vinagrete, purê e batata palha', '', 12.50, 'Lanche', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('2', 'Mega Hamburguer', 'Pão de hamburguer, duas carnes, ketchup, maionese, bacon, tomate, picles', '', 20.00, 'Lanche', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('3', 'Pastel', 'Pastel com o sabor a escolha do cliente', '', 10.00, 'Lanche', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('4', 'Coxinha', 'Coxinha de frango com catupiry', '', 2.50, 'Lanche', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('5', 'Risoli', 'Risoli de presunto com queijo', '', 3.00, 'Lanche', utc_timestamp, utc_timestamp, true);
 
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('6', 'Batata frita', 'Porção de batata frita', '', 5.00, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('7', 'Batata frita com bacon e cheddar', 'Porção de batata frita com bacon e cheddar', '', 10.00,
-        'Acompanhamento', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('8', 'Onions ', 'Porção de anéis de cebola empanadas', '', 8.50, 'Acompanhamento', utc_timestamp, utc_timestamp,
-        true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('9', 'Molho especial', 'Molho caseiro com receita do chef da casa', '', 5.00, 'Acompanhamento', utc_timestamp,
-        utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('10', 'Iscas de frango', 'Iscas de frango empanada', '', 7.00, 'Acompanhamento', utc_timestamp, utc_timestamp,
-        true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('6', 'Batata frita', 'Porção de batata frita', '', 5.00, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('7', 'Batata frita com bacon e cheddar', 'Porção de batata frita com bacon e cheddar', '', 10.00, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('8', 'Onions ', 'Porção de anéis de cebola empanadas', '', 8.50, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('9', 'Molho especial', 'Molho caseiro com receita do chef da casa', '', 5.00, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('10', 'Iscas de frango', 'Iscas de frango empanada', '', 7.00, 'Acompanhamento', utc_timestamp, utc_timestamp, true);
 
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('11', 'Coca cola', 'Refrigerante de sabor cola', '', 7.00, 'Bebida', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('12', 'Fanta', 'Refrigerante de sabor laranja', '', 6.00, 'Bebida', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('13', 'Guaraná Antarctica', 'Refrigerante de sabor guaraná', '', 5.00, 'Bebida', utc_timestamp, utc_timestamp,
-        true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('14', 'Suco', 'Suco de polpa de fruta', '', 4.50, 'Bebida', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('15', 'Água Mineral', 'Água proviniente de fontes naturais', '', 3.00, 'Bebida', utc_timestamp, utc_timestamp,
-        true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('11', 'Coca cola', 'Refrigerante de sabor cola', '', 7.00, 'Bebida', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('12', 'Fanta', 'Refrigerante de sabor laranja', '', 6.00, 'Bebida', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('13', 'Guaraná Antarctica', 'Refrigerante de sabor guaraná', '', 5.00, 'Bebida', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('14', 'Suco', 'Suco de polpa de fruta', '', 4.50, 'Bebida', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('15', 'Água Mineral', 'Água proviniente de fontes naturais', '', 3.00, 'Bebida', utc_timestamp, utc_timestamp, true);
 
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('16', 'Sorvete', 'Sorvete sabores diversos', '', 3.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('17', 'Brownie', 'Delicioso Brownie Recheado de brigadeiro de chocolate', '', 5.00, 'Sobremesa', utc_timestamp,
-        utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('18', 'Milk Shake', 'Milk shake diversos sabores', '', 7.50, 'Sobremesa', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('19', 'Pudim de Leite', 'Pudim de leite condesado', '', 6.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
-insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status)
-VALUES ('20', 'Tortinha', 'Tortas doce diversos sabores', '', 3.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('16', 'Sorvete', 'Sorvete sabores diversos', '', 3.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('17', 'Brownie', 'Delicioso Brownie Recheado de brigadeiro de chocolate', '', 5.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('18', 'Milk Shake', 'Milk shake diversos sabores', '', 7.50, 'Sobremesa', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('19', 'Pudim de Leite', 'Pudim de leite condesado', '', 6.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
+insert into produto_model(id, nome, descricao, imagem, preco, categoria, data_hora_criacao, data_hora_alteracao, status) VALUES ('20', 'Tortinha', 'Tortas doce diversos sabores', '', 3.00, 'Sobremesa', utc_timestamp, utc_timestamp, true);
 
-insert into pedido_model(id, cliente_id, status_pedido, preco, status_pagamento, tempo_preparo)
-VALUES ('1', '1', 0, 29.50, 1, '00:10:00');
+insert into pedido_model(id, cliente_id,status_pedido, preco, status_pagamento, tempo_preparo) VALUES ('1', '1', 0, 29.50,  1, '00:10:00');
 
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('1', '1');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('1', '6');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('1', '11');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('1', '16');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('1', '1');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('1', '6');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('1', '11');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('1', '16');
 
-insert into pedido_model(id, cliente_id, status_pedido, preco, status_pagamento, tempo_preparo)
-VALUES ('2', '2', 2, 35.00, 0, '00:15:00');
+insert into pedido_model(id, cliente_id,status_pedido, preco, status_pagamento, tempo_preparo) VALUES ('2', '2', 2, 35.00,  0, '00:15:00');
 
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('2', '2');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('2', '7');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('2', '13');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('2', '2');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('2', '7');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('2', '13');
 
-insert into pedido_model(id, cliente_id, status_pedido, preco, status_pagamento, tempo_preparo)
-VALUES ('3', '4', 0, 21.00, 1, '00:10:00');
+insert into pedido_model(id, cliente_id,status_pedido, preco, status_pagamento, tempo_preparo) VALUES ('3', '4', 0, 21.00,  1, '00:10:00');
 
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('3', '4');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('3', '8');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('3', '11');
-insert into pedido_produto(pedido_id, produto_id)
-VALUES ('3', '20');
-
+insert into pedido_produto(pedido_id, produto_id) VALUES ('3', '4');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('3', '8');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('3', '11');
+insert into pedido_produto(pedido_id, produto_id) VALUES ('3', '20');
